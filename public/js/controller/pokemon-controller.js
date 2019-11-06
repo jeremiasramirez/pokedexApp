f852c66718fbfc4efdb162714cbf5a4bf5daf535
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-05 20:59:09 -0400

    mejorando la seguridad en la url , y mostrado de message de errores al introducir url no coincidentes o no encontradas

--- public/js/controller/pokemon-controller.js
+++ public/js/controller/pokemon-controller.js
@@ -1,11 +1,13 @@
 pokedexApp.controller("onlyPokemon", ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams){
     //starting controller
 
+
     //back page function
     $scope.backPage = function(){
+
         setTimeout(()=>{
             history.back();
-        },400);
+        },0);
 
     };
 
@@ -30,10 +32,13 @@ pokedexApp.controller("onlyPokemon", ["$scope", "$http", "$routeParams", functio
         $scope.positionPokemon  =null;
 
        for(let i=0; i<res.data.length; i++){
+
            //verifying if the url name is finded
            if(res.data[i].name === $scope.nameUrlPokemon){
+
                $scope.findedPokemon = 1;
                $scope.positionPokemon = i;
+
            }
        }
        //if url name is fined ? show data
@@ -50,6 +55,12 @@ pokedexApp.controller("onlyPokemon", ["$scope", "$http", "$routeParams", functio
                //updating scope
                $scope.$apply();
 
+               if(window.screenY >=10){
+                   floatNotificationError("No se ha encontrado nada con: "+$routeParams.pokemonName)
+               }
+               else if (window.screenY < 10){
+                   windowError("Url invalida", "No se ha encontrado nada con: '"+$routeParams.pokemonName+"'")
+               }
 
            //timing execute
            },800)
